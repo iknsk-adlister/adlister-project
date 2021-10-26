5370deb1880a4b009bba2d7f8d7c4d27a5fc515b
sh3lbsd <86627394+sh3lbsd@users.noreply.github.com>
2021-10-26 14:37:06 -0500

    added home.jsp and HomeServlet

---
+++ src/main/java/com/codeup/adlister/controllers/HomeServlet.java
@@ -0,0 +1,19 @@
+package com.codeup.adlister.controllers;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "HomeServlet", value = "/home")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/home.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
