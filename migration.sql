4d09d930be21622b341a714acdaaf9865a4fc831
Kyle Schrader <kyle.schrader97@gmail.com>
2021-10-26 11:47:01 -0500

    added to migration.sql

--- migration.sql
+++ migration.sql
@@ -2,12 +2,16 @@ USE adlister_db;
 
 DROP TABLE IF EXISTS ads;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS ad_category;
 
 CREATE TABLE users (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     username VARCHAR(240) NOT NULL,
     email VARCHAR(240) NOT NULL,
     password VARCHAR(255) NOT NULL,
+    positive_rating INT UNSIGNED NOT NULL,
+    negative_rating INT UNSIGNED NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -19,4 +23,16 @@ CREATE TABLE ads (
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(id)
         ON DELETE CASCADE
+);
+
+CREATE TABLE categories (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(240) NOT NULL
+);
+
+CREATE TABLE ad_category (
+    ad_id INT UNSIGNED NOT NULL,
+    category_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (ad_id) REFERENCES ads(id),
+    FOREIGN KEY (category_id) REFERENCES  categories(id)
 );
